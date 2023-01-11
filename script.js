19f5c2ef485aafd6b559fe50576b542e49131af3
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-11 12:46:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+
+const sharableUrl = document.getElementById('url');
+const name = document.getElementById('name');
+const year = document.getElementById('year');
+
+const submitHandler = () => {
+    // e.preventDefault();  
+    const nameData = `name=${name.value}`;
+    const yearData = `year=${year.value}`;
+    let ans ='?'
+    if (name.value.trim() !== "") {
+        ans = ans + nameData;
+    }
+    if (name.value.trim() !== "" && year.value > 0) {
+        ans = ans + "&" + yearData;
+    }
+    if (name.value.trim() === "" && year.value >0) {
+        ans = ans + yearData;
+    }
+    const text = sharableUrl.innerText;
+    sharableUrl.innerText = text + ans;
+   
+}
